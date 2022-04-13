8d43bb46f0f0c31c1f48526b5d38325d51159d71
LeandroAgustinFernandez <leandroa.fernandez@gmail.com>
2022-04-13 12:16:26 -0300

    Adding Buttons Actions And Styles

--- README.md
+++ README.md
@@ -4,3 +4,5 @@
 - Create a 16x16 grid with JavaScript.
 - Change grid color when mouse enters.
 - Create a Reset button that clean the grid.
+- Create two buttons for change grayscale to random colors and viceversa.
+- Add some styles.
--- js/scrypt.js
+++ js/scrypt.js
@@ -1,41 +1,67 @@
 let squaresPerSide = 16;
+let typeColor = "grayscale";
 
-const gridContainer = document.querySelector('.grid_container');
-const buttonReset = document.querySelector('.btn_reset')
+const gridContainer = document.querySelector(".grid_container");
+const buttonReset = document.querySelector(".btn_reset");
+const buttonsColorType = document.querySelectorAll(".color_type");
 
-buttonReset.addEventListener('click',()=>{
-    squaresPerSide = prompt('Set number of squares per side for the new grid',16);
-    if (!squaresPerSide) {
-        squaresPerSide = 16;
-    }
-    if (squaresPerSide > 100) {
-        console.log('Sory, the max number number of squares is 100');
-        squaresPerSide = 100;
-    }
-    createGrid(squaresPerSide);
-})
+buttonsColorType.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    typeColor = e.target.dataset.type;
+  });
+});
+
+buttonReset.addEventListener("click",setNewGrid);
+
+function setNewGrid() {
+  squaresPerSide = prompt(
+    "Set number of squares per side for the new grid",
+    16
+  );
+  if (!squaresPerSide) {
+    squaresPerSide = 16;
+  }
+  if (squaresPerSide > 100) {
+    console.log("Sory, the max number number of squares is 100");
+    squaresPerSide = 100;
+  }
+  createGrid(squaresPerSide);
+}
 
 function createGrid(squaresPerSide) {
-    gridContainer.innerHTML = '';
-    gridContainer.style.gridTemplateColumns = `repeat(${squaresPerSide}, 1fr)`;
-    gridContainer.style.gridTemplateRows = `repeat(${squaresPerSide}, 1fr)`;
-    let grid = squaresPerSide * squaresPerSide;
-    for (let index = 0; index < grid; index++) {
-        const div = document.createElement('div');
-        div.classList.add('square')
-        // div.textContent = index;
-        gridContainer.appendChild(div);
-    }
-    setChangeColor();
+  gridContainer.innerHTML = "";
+  gridContainer.style.gridTemplateColumns = `repeat(${squaresPerSide}, 1fr)`;
+  gridContainer.style.gridTemplateRows = `repeat(${squaresPerSide}, 1fr)`;
+  let grid = squaresPerSide * squaresPerSide;
+  for (let index = 0; index < grid; index++) {
+    const div = document.createElement("div");
+    div.classList.add("square");
+    div.dataset.opacity = 0;
+    // div.textContent = index;
+    gridContainer.appendChild(div);
+  }
+  setChangeColor();
 }
 
 function setChangeColor() {
-    const squares = document.querySelectorAll('.square');
-    squares.forEach(square => {
-        square.addEventListener('mouseover',(e)=>{
-            e.target.style.backgroundColor = 'black'
-        })
-    });    
+  const squares = document.querySelectorAll(".square");
+  squares.forEach((square) => {
+    square.addEventListener("mouseover", changeColor);
+  });
 }
 
+function changeColor(e) {
+  if (typeColor == "grayscale") {
+    let opacity = Number(e.target.dataset.opacity);
+    opacity < 1
+      ? (e.target.dataset.opacity = opacity + 0.1)
+      : (e.target.dataset.opacity = 1);
+    e.target.style.backgroundColor = `rgba(0,0,0,${e.target.dataset.opacity})`;
+  } else {
+    e.target.dataset.opacity = 0;
+    e.target.style.backgroundColor = `rgb(${Math.floor(
+      Math.random() * 255
+    )},${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)})`;
+  }
+}
 createGrid(squaresPerSide);
