84c09b9b2f730e1f2aa2a9f8e8083e5246fc8ee6
LeandroAgustinFernandez <leandroa.fernandez@gmail.com>
2022-04-13 11:38:06 -0300

    Dinamic Grid Generator

--- js/scrypt.js
+++ js/scrypt.js
@@ -1,13 +1,29 @@
+let squaresPerSide = 16;
+
 const gridContainer = document.querySelector('.grid_container');
-function createGrid() {
-    gridContainer.setAttribute('style','display:grid');
-    gridContainer.style.gridTemplateColumns = 'repeat(16, 1fr)';
-    gridContainer.style.gridTemplateRows = 'repeat(16, 1fr)';
-    for (let index = 0; index < 16 * 16; index++) {
+const buttonReset = document.querySelector('.btn_reset')
+
+buttonReset.addEventListener('click',()=>{
+    squaresPerSide = prompt('Set number of squares per side for the new grid',16);
+    if (!squaresPerSide) {
+        squaresPerSide = 16;
+    }
+    if (squaresPerSide > 100) {
+        console.log('Sory, the max number number of squares is 100');
+        squaresPerSide = 100;
+    }
+    createGrid(squaresPerSide);
+})
+
+function createGrid(squaresPerSide) {
+    gridContainer.innerHTML = '';
+    gridContainer.style.gridTemplateColumns = `repeat(${squaresPerSide}, 1fr)`;
+    gridContainer.style.gridTemplateRows = `repeat(${squaresPerSide}, 1fr)`;
+    let grid = squaresPerSide * squaresPerSide;
+    for (let index = 0; index < grid; index++) {
         const div = document.createElement('div');
         div.classList.add('square')
-        div.textContent = index;
-        div.style.cssText = 'border: 1px solid black'
+        // div.textContent = index;
         gridContainer.appendChild(div);
     }
     setChangeColor();
@@ -22,4 +38,4 @@ function setChangeColor() {
     });    
 }
 
-createGrid();
+createGrid(squaresPerSide);
